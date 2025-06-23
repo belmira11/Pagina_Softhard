555d51923f5d994a6e1e1ad51b261ad44c963c61
belmira11 <belmirarodrigues15@gmail.com>
2025-06-23 14:32:21 +0100

    Atualizando projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-
+import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import Banner from "@/components/Banner";
 import ChatButton from "@/components/ChatButton";
@@ -12,17 +12,27 @@ import Equipe from "@/components/Equipe"
 export default function Home() {
   return (
     <>
-     <Navbar/>  
-     <Banner/>
-     <ChatButton/>
-       <AboutSobre/>
-       <Testemunha/>
-        <Servico/>
-        <HowWork/>
-         <Trabalhamos/>
-         <Portfolio/>
-         <Equipe/>
-     </>
+      <Head>
+        <title>Softhard - Tecnologia Comunicação</title>
+        <meta name="description" content="Encontre conosco um departamento de marketing externo para a sua empresa." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta property="og:title" content="Softhard - Tecnologia Comunicação" />
+        <meta property="og:description" content="Encontre conosco um departamento de marketing externo para a sua empresa." />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="pt_BR" />
+      </Head>
+      <Navbar/>  
+      <Banner/>
+      <ChatButton/>
+      <AboutSobre/>
+      <Testemunha/>
+      <Servico/>
+      <HowWork/>
+      <Trabalhamos/>
+      <Portfolio/>
+      <Equipe/>
+    </>
   );
 }
 
